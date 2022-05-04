d20622970d7807caa216d0c005d62a8a232c1ff2
Nicolae Pop <nicupop729@gmail.com>
2022-05-04 17:26:44 -0400

    Update: post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+
+  def recent_posts
+    Post.order(created_at: :desc).limit(3)  
+  end
 end
