3d4867c4a0bee47f8d2a3da8743acc68f414947a
Amrhub <amr_ahmed65@hotmail.com>
2022-05-12 22:44:02 +0200

    Add: profile_pic to users using active_storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :requester_ids, class_name: 'Friendship', foreign_key: 'requester_id'
   has_many :requestee_ids, class_name: 'Friendship', foreign_key: 'requestee_id'
+  has_one_attached :profile_pic
 end
