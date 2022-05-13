2aa19668c460ff5434cc1afccfcb7b24e82a2efe
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-05-13 21:48:37 +0200

    Override: devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: {
+      sessions: "users/sessions",
+      registrations: "users/registrations",
+    }
+
+  get "/member-data", to: "members#show"
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      get 'device_data/show_avg_hourly_data' => 'device_data#show_avg_hourly_data'
+      get "device_data/show_avg_hourly_data" => "device_data#show_avg_hourly_data"
       resources :observations
       resources :doctors
       resources :patients
