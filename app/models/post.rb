4555e8c6d5bc7d37f7c78cd15bb253ec9768c5ce
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-05-22 09:50:02 +0200

    Feat: def  likes_counter and comments_counter to 0

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  before_save :intialize_likes_comments_counter
 
   def recent_posts
     Post.order(created_at: :desc).limit(3)
+  end
+
+  def intialize_likes_comments_counter
+    self.likes_counter = 0
+    self.comments_counter = 0
   end
 end
